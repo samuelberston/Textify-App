90c691075de3a502244801fc64f9b26a8ca175c7
Sam Berston <samuelberston@gmail.com>
2021-03-29 16:44:47 -0700

    MySQL table loads dummy data

--- schema.sql
+++ schema.sql
@@ -7,14 +7,14 @@ USE MVP;
 CREATE TABLE msgs (
   message_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
-  to INT NOT NULL,
+  receiver VARCHAR(12) NOT NULL,
   -- from INT NOT NULL,
   -- text VARCHAR(255) NOT NULL,
   cron VARCHAR(12) NOT NULL
 );
 
-INSERT INTO TABLE (title, to, time) VALUES ('trash', '4151234567', '* * * * * *');
-INSERT INTO TABLE (title, to, time) VALUES ('trash', '4151234567', 'time');
-INSERT INTO TABLE (title, to, time) VALUES ('trash', '4151234567', 'time');
-INSERT INTO TABLE (title, to, time) VALUES ('trash', '4151234567', 'time');
-INSERT INTO TABLE (title, to, time) VALUES ('trash', '4151234567', 'time');
+INSERT INTO msgs (message_id, title, receiver, cron) VALUES (1, 'trash', '4151234567', '* * * * * *');
+INSERT INTO msgs (message_id, title, receiver, cron) VALUES (2, 'trash', '4151234567', 'time');
+INSERT INTO msgs (message_id, title, receiver, cron) VALUES (3, 'trash', '4151234567', 'time');
+INSERT INTO msgs (message_id, title, receiver, cron) VALUES (4, 'trash', '4151234567', 'time');
+INSERT INTO msgs (message_id, title, receiver, cron) VALUES (5, 'trash', '4151234567', 'time');
