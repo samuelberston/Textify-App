b06b1ce5d0369ac0582ef99e42b812eaefbbd21f
Sam Berston <samuelberston@gmail.com>
2021-03-29 16:49:30 -0700

    set up MySQL msgs table with all six fields

--- schema.sql
+++ schema.sql
@@ -7,14 +7,14 @@ USE MVP;
 CREATE TABLE msgs (
   message_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
-  receiver VARCHAR(12) NOT NULL,
-  -- from INT NOT NULL,
-  -- text VARCHAR(255) NOT NULL,
+  receiver VARCHAR(14) NOT NULL,
+  sender VARCHAR(14) NOT NULL,
+  text VARCHAR(255) NOT NULL,
   cron VARCHAR(12) NOT NULL
 );
 
-INSERT INTO msgs (message_id, title, receiver, cron) VALUES (1, 'trash', '4151234567', '* * * * * *');
-INSERT INTO msgs (message_id, title, receiver, cron) VALUES (2, 'trash', '4151234567', 'time');
-INSERT INTO msgs (message_id, title, receiver, cron) VALUES (3, 'trash', '4151234567', 'time');
-INSERT INTO msgs (message_id, title, receiver, cron) VALUES (4, 'trash', '4151234567', 'time');
-INSERT INTO msgs (message_id, title, receiver, cron) VALUES (5, 'trash', '4151234567', 'time');
+INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (1, 'trash', '+14151234567', '+14158468793', 'text', '* * * * *');
+INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (2, 'trash', '+14151234567', '+14158468793', 'text', '* * * * *');
+INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (3, 'trash', '+14151234567', '+14158468793', 'text', 'time');
+INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (4, 'trash', '+14151234567', '+14158468793', 'text', 'time');
+INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (5, 'trash', '+14151234567', '+14158468793', 'text', 'time');
