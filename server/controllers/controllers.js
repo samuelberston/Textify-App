2059a970cc0c0eacbab1107188e954a39e4e44da
Sam Berston <samuelberston@gmail.com>
2021-03-29 18:29:05 -0700

    wrote cron function

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,13 +1,27 @@
 function cronCode(timeObj) {
+  let cron = '';
   // convert time object into (five star) cron code
-  const { dayOfMonth, dayOfWeek, month, time } = timeObj;
-  // dayOfMonth: "1"
-  // dayOfWeek: "Wednesday"
-  // month: "2021-03"
-  // time: "17:34"
+  const { time } = timeObj;
+  let {
+    dayOfMonth, dayOfWeek, month,
+  } = timeObj;
   const minute = parseInt(time.split(':')[1], 10);
   const hour = parseInt(time.split(':')[0], 10);
 
+  if (dayOfWeek === null) {
+    dayOfWeek = '*';
+  }
+
+  if (dayOfMonth === null) {
+    dayOfMonth = '*';
+  }
+
+  if (month === null) {
+    month = '*';
+  }
+
+  cron = [minute, hour, dayOfWeek, dayOfMonth, month].join(' ');
+  return cron;
 }
 
 module.exports = cronCode;
