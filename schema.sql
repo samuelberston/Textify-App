75df119e0eb6569671561665370cee42bbef1d6d
Sam Berston <samuelberston@gmail.com>
2021-03-30 03:34:54 -0700

    contacts table and seed

--- schema.sql
+++ schema.sql
@@ -17,3 +17,15 @@ INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (1, "t
 INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (2, "good morning", "+14151234567", "+14158468793", "good morning beautiful !", "0 24 * 3 September");
 INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (3, "Rhonda", "+14151234567", "+14158468793", "Did you remember to water the plants??", "45 16 Tuesday, Wednesday * *");
 INSERT INTO msgs (message_id, title, receiver, sender, text, cron) VALUES (4, "reminder", "+14151234567", "+14158468793", "buy tickets to edc", "* * * * *");
+
+CREATE TABLE contacts (
+  contact_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  firstName VARCHAR(25) NOT NULL,
+  lastName VARCHAR(25) NOT NULL,
+  number VARCHAR(12) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
+
+INSERT INTO contacts (contact_id, firstName, lastName, number, email) VALUES (1, "Sam", "Berston", "+14158468793", "samuelberston@gmail.com");
+INSERT INTO contacts (contact_id, firstName, lastName, number, email) VALUES (2, "Mariah", "Carey", "+12129276505", "mariahcarey@gmail.com");
+INSERT INTO contacts (contact_id, firstName, lastName, number, email) VALUES (3, "Cardi", "B", "+12128082233", "BelcalisAlmánzar@gmail.com");
