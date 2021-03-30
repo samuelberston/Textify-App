ec88d2c9a8002b11540c34f5d2f4bcef2b098cb0
Sam Berston <samuelberston@gmail.com>
2021-03-29 17:24:06 -0700

    shedule subcomponent holds data in state and send to root component on click

--- client/src/components/schedule/Time.js
+++ client/src/components/schedule/Time.js
@@ -5,20 +5,30 @@ class Time extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dayOfWeek: null,
+      month: null,
+      dayOfMonth: null,
       time: null,
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log(e.target.id);
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
   }
 
   render() {
     const { confirmTime } = this.props;
-    const { time } = this.state;
     return (
       <div id="time">
-        <form onSubmit={confirmTime} id="timeForm">
-          <input id="dayOfWeek" type="string" placeholder="day of the week" />
-          <input id="month" type="month" placeholder="month" />
-          <input id="dayOfMonth" type="number" min="1" max="31" placeholder="day of the month" />
-          <input id="time" type="time" placeholder="time" />
+        <form onSubmit={(e) => { confirmTime(e, this.state); }} id="timeForm">
+          <input id="dayOfWeek" type="string" placeholder="day of the week" onChange={this.handleChange} />
+          <input id="month" type="month" placeholder="month" onChange={this.handleChange} />
+          <input id="dayOfMonth" type="number" min="1" max="31" placeholder="day of the month" onChange={this.handleChange} />
+          <input id="time" type="time" placeholder="time" onChange={this.handleChange} />
           <input type="submit" value="confirm" />
         </form>
       </div>
