be487ca4fe8acfcc3d4a3f6ed492481eb4df2031
Sam Berston <samuelberston@gmail.com>
2021-03-31 14:09:30 -0700

    refactored schedule form

--- client/src/components/schedule/Schedule.js
+++ client/src/components/schedule/Schedule.js
@@ -10,25 +10,40 @@ class Schedule extends React.Component {
     super(props);
 
     this.state = {
-      content: null,
+      title: null,
+      to: null,
+      text: null,
+      dayOfWeek: null,
+      month: null,
+      dayOfMonth: null,
       time: null,
     };
-
+    this.handleChange = this.handleChange.bind(this);
     this.postMessage = this.postMessage.bind(this);
-    this.confirmContent = this.confirmContent.bind(this);
-    this.confirmTime = this.confirmTime.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
   }
 
   postMessage(e) {
     e.preventDefault();
 
-    const { content, time } = this.state;
+    const {
+      title, to, text, dayOfWeek, month, dayOfMonth, time,
+    } = this.state;
     const body = {
-      title: content.title,
-      receiver: content.to,
-      sender: content.from,
-      text: content.text,
-      time,
+      title,
+      receiver: to,
+      text,
+      time: {
+        dayOfWeek,
+        month,
+        dayOfMonth,
+        time,
+      },
     };
 
     axios.post('/messages', body)
@@ -38,22 +53,6 @@ class Schedule extends React.Component {
       .catch((err) => {
         throw err;
       });
-  }
-
-  confirmContent(e, content) {
-    e.preventDefault();
-
-    this.setState({
-      content,
-    });
-  }
-
-  confirmTime(e, time) {
-    e.preventDefault();
-
-    this.setState({
-      time,
-    });
   }
 
   render() {
@@ -69,8 +68,45 @@ class Schedule extends React.Component {
           <i className="fa fa-clock" />
         </div>
         <div id="schedule">
-          <Content confirmContent={this.confirmContent} />
-          <Time confirmTime={this.confirmTime} />
+          <form>
+            <div id="content">
+              <input id="title" type="text" placeholder="title" onChange={this.handleChange} required />
+              <label>
+                to:
+                &nbsp;
+                <input id="to" type="number" placeholder="(123) 456-7890" onChange={this.handleChange} required />
+              </label>
+              <input id="text" type="text" placeholder="Good morning, love!" onChange={this.handleChange} required />
+            </div>
+            <div id="time">
+              <select id="month" type="string" onChange={this.handleChange}>
+                <option value="January">January</option>
+                <option value="February">February</option>
+                <option value="March">March</option>
+                <option value="April">April</option>
+                <option value="May">May</option>
+                <option value="June">June</option>
+                <option value="July">July</option>
+                <option value="August">August</option>
+                <option value="September">September</option>
+                <option value="October">October</option>
+                <option value="November">November</option>
+                <option value="December">December</option>
+              </select>
+              <input id="dayOfMonth" type="number" min="1" max="31" placeholder="day of the month" onChange={this.handleChange} />
+              <select id="dayOfWeek" type="string" onChange={this.handleChange}>
+                <option id="dayOfWeek" value="Monday">Monday</option>
+                <option id="dayOfWeek" value="Tuesday">Tuesday</option>
+                <option id="dayOfWeek" value="Wednesday">Wednesday</option>
+                <option id="dayOfWeek" value="Thursday">Thursday</option>
+                <option id="dayOfWeek" value="Friday">Friday</option>
+                <option id="dayOfWeek" value="Saturday">Saturday</option>
+                <option id="dayOfWeek" value="Sunday">Sunday</option>
+              </select>
+              <input id="time" type="time" placeholder="time" onChange={this.handleChange} required />
+              <input type="submit" value="confirm" />
+            </div>
+          </form>
         </div>
         <div id="scheduleButton">
           <input type="button" value="schedule" onClick={this.postMessage} />
