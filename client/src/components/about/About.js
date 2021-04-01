4b6be1a3ecc3b03be49af916c1eb9771e213d4e4
Sam Berston <samuelberston@gmail.com>
2021-03-31 20:24:30 -0700

    style github

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -7,14 +7,12 @@ const About = () => (
         About
       </h3>
     </div>
-    <div id="about">
-      <p>
-        schedule automated text messages with Textify
-      </p>
-      <a id="gitHubLink" href="https://github.com/samuelberston/MVP" target="_blank" rel="noreferrer">
-        <i className="fab fa-github" title="GitHub Icon" />
-      </a>
-    </div>
+    <p>
+      schedule automated text messages with Textify
+    </p>
+    <a id="gitHubLink" href="https://github.com/samuelberston/MVP" target="_blank" rel="noreferrer">
+      <i className="fab fa-github" title="GitHub Icon" />
+    </a>
   </div>
 );
 
