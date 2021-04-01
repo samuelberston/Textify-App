5246103ef3ef8ed79b6bbf448e34670830f0136f
Sam Berston <samuelberston@gmail.com>
2021-03-31 23:34:10 -0700

    about style

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -7,9 +7,21 @@ const About = () => (
         About
       </h3>
     </div>
-    <p>
-      schedule automated text messages with Textify
-    </p>
+    <div id="about">
+      <p>
+        Textify allows users to schedule automated text messages.
+      </p>
+      <p>
+        Textify uses the Twilio API to send SMS messages and
+        the node-cron library to schedule tasks.
+      </p>
+      <p>
+        The front end is build with JavaScript and React.
+      </p>
+      <p>
+        The back end is build with Nodejs, Express, and MySQL.
+      </p>
+    </div>
     <a id="gitHubLink" href="https://github.com/samuelberston/MVP" target="_blank" rel="noreferrer">
       <i className="fab fa-github" title="GitHub Icon" />
     </a>
